1e80c4f339d6732b54df97715409c44f9f8a4b3d
HagaiHargil <hagaihargil@gmail.com>
2018-04-14 20:21:46 +0300

    iterate_file is working, pre-refactoring

---
+++ src/main.rs
@@ -0,0 +1,20 @@
+extern crate binary_lst_parser;
+
+use binary_lst_parser::parse_lst;
+use std::collections::HashMap;
+
+fn main() {
+    let mut starts_map = HashMap::new();
+    starts_map.insert("trial1.lst", 2usize);
+    starts_map.insert("4-byte006.lst", 1480usize);
+    let fname = String::from("4-byte006.lst");
+    let start_of_data = starts_map[fname.as_str()];
+    let range = 512u64;
+//    let timepatch = String::from("0");
+    let channel_map = [0, 0, 0, 0, 0, 1];
+    let timepatch = String::from("5");
+    let res = parse_lst(fname, start_of_data, range,
+                                                                timepatch, channel_map);
+
+    println!("Done.")
+}
